75414f5b9270bd6465f7c9c3a08bb85aa8717898
Abhishek <officialabhishek0808@gmail.com>
2026-09-19 21:39:06 +0530

    opps concept started here.

---
+++ OOPs/car.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, modal, year, top_speed, color):
+        self.modal = modal
+        self.year = year
+        self.top_speed = top_speed
+        self.color = color
---
+++ OOPs/classes.py
@@ -0,0 +1,9 @@
+from car import Car
+
+car1 = Car("Tata Punch", 2026, 180, "red")
+car2 = Car("Bugati", 2026, 300, "White")
+
+print('this is cars speed: ', car1.top_speed, car2.top_speed)
+print(car1.year, car2.year)
+print(car1.color, car2.color)
+print(car1.modal, car2.modal)
